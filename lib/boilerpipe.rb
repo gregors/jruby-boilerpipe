eb2d72fef02fb303e766852d644c79a40e1f2a7d
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-02 13:28:12 -0400

    move article_extrator to own file

--- lib/boilerpipe.rb
+++ lib/boilerpipe.rb
@@ -1,26 +1,3 @@
 require 'boilerpipe/version'
 require_relative 'boilerpipe-common-2.0-SNAPSHOT-jar-with-dependencies.jar'
-
-module Boilerpipe
-  java_import 'com.kohlschutter.boilerpipe.extractors.ArticleExtractor'
-  java_import 'com.kohlschutter.boilerpipe.util.UnicodeTokenizer'
-  java_import java.net.URL
-
-  class ArticleExtractor
-    def  self.get_text(s)
-      url = nil
-
-      begin
-        url = Java::JavaNet::URL.new(s)
-      rescue Java::JavaNet::MalformedURLException => e
-        # not a URL
-      end
-      input = url ? url : s
-      ArticleExtractor::INSTANCE.get_text(input)
-    end
-
-    class <<self  
-      alias_method :text, :get_text
-    end 
-  end
-end
+require 'boilerpipe/extractors/article_extractor'
---
+++ lib/boilerpipe/extractors/article_extractor.rb
@@ -0,0 +1,23 @@
+module Boilerpipe
+  java_import 'com.kohlschutter.boilerpipe.extractors.ArticleExtractor'
+  java_import 'com.kohlschutter.boilerpipe.util.UnicodeTokenizer'
+  java_import java.net.URL
+
+  class ArticleExtractor
+    def  self.get_text(s)
+      url = nil
+
+      begin
+        url = Java::JavaNet::URL.new(s)
+      rescue Java::JavaNet::MalformedURLException => e
+        # not a URL
+      end
+      input = url ? url : s
+      ArticleExtractor::INSTANCE.get_text(input)
+    end
+
+    class <<self  
+      alias_method :text, :get_text
+    end 
+  end
+end
