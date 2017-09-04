43c5fa5c5d555f3c26a71864224eabcfdacf1602
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-04 14:29:22 -0400

    add TextBlock & TextDocument

--- lib/boilerpipe.rb
+++ lib/boilerpipe.rb
@@ -1,4 +1,5 @@
 require_relative 'boilerpipe-common-2.0-SNAPSHOT-jar-with-dependencies.jar'
 require 'boilerpipe/version'
 require 'boilerpipe/sax/boilerpipe_html_parser'
+require 'boilerpipe/document/document'
 require 'boilerpipe/extractors/article_extractor'
---
+++ lib/boilerpipe/document/document.rb
@@ -0,0 +1,6 @@
+module Boilerpipe
+  module Document
+    java_import 'com.kohlschutter.boilerpipe.document.TextDocument'
+    java_import 'com.kohlschutter.boilerpipe.document.TextBlock'
+  end
+end
