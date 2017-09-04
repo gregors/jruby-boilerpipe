8415b8caa717266f75ef54740b6fdf00fc5529a3
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-04 18:45:41 -0400

    add a mapping from ruby symbol to java internal default label when querying a TextBlock from ruby

--- lib/boilerpipe/document/document.rb
+++ lib/boilerpipe/document/document.rb
@@ -2,5 +2,15 @@ module Boilerpipe
   module Document
     java_import 'com.kohlschutter.boilerpipe.document.TextDocument'
     java_import 'com.kohlschutter.boilerpipe.document.TextBlock'
+
+    class TextBlock
+      # Adding a mapping from ruby symbols to the format string used on the java side
+      # e.g. de.l3s.boilerpipe/INDICATES_END_OF_TEXT is not the same as INDICATES_END_OF_TEXT
+      # This is only for when we do TextBlock#has_label? from jruby
+      def has_label?(l)
+        l = "de.l3s.boilerpipe/#{l.to_s}" if l.is_a?(Symbol)
+        self.hasLabel(l)
+      end
+    end
   end
 end
