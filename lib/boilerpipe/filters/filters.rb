a781527e1d6579873dced4102a4f0322e5b47806
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-09-05 15:01:25 -0400

    add more filters ExpandTitleToContentFilter KeepLargestBlockFilter LargeBlockSameTagLevelToContentFilter ListAtEndFilter BoilerplateBlockFilter

--- lib/boilerpipe/filters/filters.rb
+++ lib/boilerpipe/filters/filters.rb
@@ -6,7 +6,18 @@ module Boilerpipe
     java_import 'com.kohlschutter.boilerpipe.filters.english.HeuristicFilterBase'
     java_import 'com.kohlschutter.boilerpipe.filters.heuristics.BlockProximityFusion'
     java_import 'com.kohlschutter.boilerpipe.filters.heuristics.DocumentTitleMatchClassifier'
+    java_import 'com.kohlschutter.boilerpipe.filters.heuristics.ExpandTitleToContentFilter'
+    java_import 'com.kohlschutter.boilerpipe.filters.heuristics.KeepLargestBlockFilter'
+    java_import 'com.kohlschutter.boilerpipe.filters.heuristics.LargeBlockSameTagLevelToContentFilter'
+    java_import 'com.kohlschutter.boilerpipe.filters.heuristics.ListAtEndFilter'
     java_import 'com.kohlschutter.boilerpipe.filters.heuristics.TrailingHeadlineToBoilerplateFilter'
+    java_import 'com.kohlschutter.boilerpipe.filters.simple.BoilerplateBlockFilter'
+
+    class ExpandTitleToContentFilter
+      def self.process(doc)
+        new.process(doc)
+      end
+    end
 
     class IgnoreBlocksAfterContentFilter
       def self.process(doc)
